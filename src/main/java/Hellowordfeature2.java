e147997fb48d63f0f06d8e1bb88e1108e97e29e4
ajaybose2304 <ajay@cozysystems.net>
2021-02-15 19:31:39 -0500

    Made changes in Feature2

---
+++ src/main/java/Hellowordfeature2.java
@@ -0,0 +1,6 @@
+public class Hellowordfeature2 {
+    static void main(String[] args){
+        System.out.println("Hello World in Feature2!");
+        System.out.println("This will be retained");
+    }
+}
